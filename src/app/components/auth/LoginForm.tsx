4db03dafae875203bde3b54c36c732b9f917131d
Tarun Kumar <adalertsmanager@gmail.com>
2025-08-18 17:58:28 +0530

    added

--- src/app/components/auth/LoginForm.tsx
+++ src/app/components/auth/LoginForm.tsx
@@ -8,6 +8,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Card, CardContent } from "@/components/ui/card";
+import { Star } from "lucide-react";
 
 import {
   Form,
