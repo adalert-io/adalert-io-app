ffebeacac83308dcca2b0b69a0a3e3738ec90c50
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2025-09-05 03:41:32 +0800

    disabled pdf button if no more than 10 alerts

--- src/app/dashboard/Dashboard.tsx
+++ src/app/dashboard/Dashboard.tsx
@@ -1736,7 +1736,7 @@ export default function Dashboard() {
                   variant='outline'
                   size='icon'
                   className='relative hidden'
-                  disabled={isGeneratingPdf}
+                  disabled={isGeneratingPdf || alerts.length < 10}
                   onClick={async () => {
                     if (!selectedAdsAccount) return;
                     setIsGeneratingPdf(true);
