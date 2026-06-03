f8cf8dbf8dc1793db467139e3334517cf2bd338a
Iammohitwalia <146377191+adalert-io@users.noreply.github.com>
2026-06-03 13:02:16 +0530

    .

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -9,7 +9,7 @@ function Textarea({
     <textarea
       data-slot="textarea"
       className={cn(
-        "border-input placeholder:text-muted-foreground focus-visible:border-blue-400 focus-visible:ring-[3px] focus-visible:ring-blue-400/50 flex min-h-20 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs outline-none md:text-sm",
+        "border-input placeholder:text-muted-foreground focus-visible:border-blue-400 focus-visible:ring-[3px] focus-visible:ring-blue-400/50 flex min-h-20 w-full rounded-md border bg-transparent px-3 py-2 text-base shadow-xs outline-none max-md:text-base md:text-sm",
         className,
       )}
       {...props}
